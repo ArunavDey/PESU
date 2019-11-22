65abfdaed972e3e98cde98261f7f3ba6b7dc2e1e
Arunav <51881303+ArunavDey@users.noreply.github.com>
2019-11-22 08:31:06 +0530

    update finalcode.py

--- finalcode.py
+++ finalcode.py
@@ -3,8 +3,28 @@ import pandas as pd
 import pathlib as pl
 import time
 import sys
-from simplecrypt import encrpt, decrpyt 
-from getpass import getpass
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "w+") as key_file:
+        key_file.write(key)
+def load_key():
+    return open("key.key", "r+").read()
+write_key()
+key = load_key()
+msg = "message".encode()
+f =Fernet(key)
+encrypted= f.encrypt(msg)
+print(encrypted)
+def encrypt(filename, key):
+    f = Fernet(key)
+    with open(filename, "r+") as file:
+        file_data = file.read()
+    encrypted_data = f.encrypt(file_data)
+    with open(filename, "w+") as file:
+        file.write(encrypted_data)
+def decrypt(filename, key):
+    """
 book=[['Advanced Engineering Mathematics','Erwin Kreyzig','maths','420'],['Higher Engineering Mathematics','BS Grewal','maths','69'],['Higher Engineering Mathematics','BV Ramana','maths','420'],['Calculus','James Stewart','maths','69'],['How to solve it by computer','RG Dromey','CSE','69'],['The C puzzle book','Alan R Feuer','CSE','420'],['The C Programming Language','Brian Kernighan and Dennis Ritchie','CSE','420'],['Expert C Programing','Peter van der Linden','CSE','69'],['Engineering Chemisry','Gadag and Nityananda shetty A','chem','420'],['Fundementals of molecular spectroscopy','Banwell','chem','69'],['Engineering Chemistry','PC jain and Monica jain','chem','420'],['Engineering Chemistry','wiley publication','chem','420'],['Industrial Electrochemistry','Pletcher and Walsh FC','chem','69'],['Atkins Physical Chemistry','Atkins and de Paula','chem','69'],['Electronic devices and circuit theory','robert L Boylestad and Louis Nashelsky','ECE','69'],['Digital Design with an Introduction to Verilog HDL','Morris Moano','ECE','420'],['Electronic Communication Systems,Fundamentals through Advanced','Wayne Tomasi','ECE','420'],['Introduction to Embedded Systems','KV Shibu','ECE','420'],['ARM System Develper Guide','Andrew N Sloss','ECE','69'],['Quantum Physics of Atmos Nuclei and Molecules','Eisberg and Robert','phy','420'],['Quantum Physics','Gasiorowicz','phy','420'],['Principles of Quantum Mechanics','Sankar','phy','69'],['Lectures of Physics','Feynman,leighton and Sands','phy','420'],['Concepts of Modern Physics','Arthur','phy','69'],['Mechanics of Materials','Ferdinand Beer','mes','420'],['Elements of Mechanical Engineering','KR Gopalkrishna','mes','69'],['An Introduction to Mechanical Engineering','Michael Clifford','mes','420'],['Mechatronics:a multidisciplinary approach','W Bolton','mes','69'],['Elements of Manufacturing Processes','BS Nagendra Parashar and RK.Mittal','mes','70'],['Basic Mechanical Engineering','Pravin Kumar','mes','80'],['Basis Electrical Engineering','DC Kulshreshta','EEE','80'],['Basis Electrical Engineering','VN and Arvind Mittle','EEE','87'],['Electrical and Electronic Technology','Hughes,Brown &Smith','EEE','96']]
 with open("books.csv", "w+", newline = '') as f:
     writer = csv.writer(f, quoting = csv.QUOTE_MINIMAL)
@@ -71,6 +91,8 @@ def login():
                 writer.writerow(l)
             print("\nRegistration successful!\n")
     def mem():
+        global books2
+        global mems
         print(mems.iloc[:,[0,1,2,4,5]])
         #print((pd.read_csv("stinfo.csv")).iloc[:,[0,1,2,4,5]])
         opt = int(input("\n1.Lend books\n2.Go back\n"))
@@ -81,12 +103,14 @@ def login():
             opt1 = int(input("\nSelect user using index: "))
             print(books2)
             bk = int(input("\nSelect book using index: "))
-            #(pd.DataFrame(list(pd.read_csv("books.csv")))).at[bk, 'Stock'] -= 1
-            #(pd.DataFrame(list(pd.read_csv("stinfo.csv")))).at[opt1, 'Lent'] +=1
+            books2.at[bk, 'Stock'] -= 1
+            mems.at[opt1, 'Lent'] +=1
+            books2.to_csv("books.csv")
+            mems.to_csv("stinfo.csv")
             #(pd.read_csv("books.csv")).set_value(bk, 'Stock', (pd.read_csv("books.csv")).iloc[bk, 3]-1) 
             #(pd.read_csv("stinfo.csv")).set_value(opt1, 'Lent', (pd.read_csv("stinfo.csv")).iloc[bk, 4]+1)
-            books2.iloc[bk, 3]-=1
-            mems.iloc[opt1, 4]+=1
+            #books2.iloc[bk, 3]-=1
+            #mems.iloc[opt1, 4]+=1
             print("Book lent.")
         elif opt == 2:
             intro()
@@ -102,8 +126,8 @@ def login():
                 print("\nLogged out\n")
                 sys.exit()
             elif opt1 == 3:
-                print(books2)
-                #print((pd.read_csv("books.csv")))
+                #print(books2)
+                print((pd.read_csv("books.csv")))
             else:
                 while opt1 != 1 or opt1 != 2 or opt1 != 3:
                     print("Invalid option, try again.")
